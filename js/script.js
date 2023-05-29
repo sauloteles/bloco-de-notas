5a61daa41df896023a139c9d9c14b06bc49106f7
sauloteles <saulot3l35@gmail.com>
2023-05-29 20:11:46 -0300

    target id

--- js/script.js
+++ js/script.js
@@ -1,47 +1,36 @@
-let blocoNotas = document.getElementsByClassName("main__bloco-notas")[0];
+let blocoNotas = document.getElementById("main__bloco-notas");
 let btnAdd = document.getElementsByClassName("main__btn")[0];
 
 let fade = document.getElementsByClassName("fade")[0];
-let modal = document.getElementsByClassName("nota-complete");
+let modal = document.getElementsByClassName("nota-complete")[0];
 let posicao = 0;
 
 
 function createNote(){
     nota = document.createElement('div');
     nota.classList.add('nota')
-    nota.innerHTML = `<div class="nota-titulo"  onclick="toggleModal2(${posicao})">
-                            <p>Titulo<p>
-                        </div>
-                        <div class="nota-complete ">
-                            <div>
-                                <input class="input-title input " type="text">
-                            </div>
-                            <div>
-                                <textarea class="input-text input" name="" id="" cols="30" rows="10"></textarea>
-                            </div>
+    nota.innerHTML = `<div class="nota-titulo">
+                            <p id = ${posicao}>Titulo<p>
                         </div>`
-    
     blocoNotas.appendChild(nota)
-    fade.classList.toggle("hide");
+    toggleModal()
     posicao +=1;
-   
 }
 
 function toggleModal(){
     fade.classList.toggle("hide");
-    modal[modal.length-1].classList.toggle("hide");
-
-}
-function toggleModal2(posicao){
-    fade.classList.toggle("hide");
-    modal[posicao].classList.toggle("hide");
-
+    modal.classList.toggle("hide");
 
 }
 
 btnAdd.addEventListener('click',createNote);
 fade.addEventListener('click',toggleModal);
-// let notaTitulo = document.getElementsByClassName("nota-titulo")[0];
-// notaTitulo.addEventListener('click',toggleModal);
 
+blocoNotas.addEventListener('click', function(e) {
+    fade.classList.toggle("hide");
+    modal.classList.toggle("hide");
+    
+    console.log(e.target.id)
+
+})
    
